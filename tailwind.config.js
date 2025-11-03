18119b8a594d56ab7ec3cbd776c58d050093d374
jawidbahrami07-collab <jawidbahrami07@gmail.com>
2025-11-03 17:04:21 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        sohaGreen: "#2DB769",
+        sohaGold: "#D4AF37"
+      }
+    }
+  },
+  plugins: []
+}
